e5e7c4b165a0096ea1af4e5c78b99c7a6a33553c
Sie7he <diegoescurraromero@gmail.com>
2024-01-09 23:08:21 -0300

    nuevo proyecto

--- src/components/Views/Hero.jsx
+++ src/components/Views/Hero.jsx
@@ -9,7 +9,7 @@ export const Hero = () => {
     <div className="grid max-w-screen-xl px-4 py-8 mx-auto lg:gap-8 xl:gap-0 lg:py-16 lg:grid-cols-12">
         <div className="mr-auto place-self-center lg:col-span-7">
             <h1 className="max-w-2xl mb-4 text-4xl font-bold tracking-tight leading-none md:text-5xl xl:text-6xl text-shadowc">Hola, mi nombre es Diego Escurra</h1>
-            <p className="max-w-[34rem] mb-6 font-light text-arsenic lg:mb-8 md:text-lg lg:text-xl "><strong>Ingeniero Informático </strong>con conocimientos en <strong>Desarrollo Web Full Stack y Análisis de Sistemas.</strong> </p>
+            <p className="max-w-[35rem] mb-6 font-light text-arsenic lg:mb-8 md:text-lg lg:text-xl "><strong>Ingeniero Informático </strong>con conocimientos en <strong>Desarrollo Web Full Stack y Análisis de Sistemas</strong>.</p>
             <div className='flex gap-4 w-full h-14 '>
             <a className='h-10 w-10' target='_blank' rel='noreferrer' href='https://www.linkedin.com/in/diego-escurra-6978651ba/'>
               <LinkedinIcon />
--- src/data/projects.js
+++ src/data/projects.js
@@ -2,16 +2,16 @@
 export const projects = [
 
     {
-        id: 2,
-        img: '/metas.png',
-        title: 'Metas app',
-        href: 'https://github.com/Sie7he/academiaX',
-        icon: ['NodeJSIcon', 'ReactJsIcon', 'PostgreSQLIcon'],
-        buttontxt: 'Visitar Repositorio',
-        description: 'Aplicación interactiva CRUD para el registro y seguimiento de metas diarias, semanales y mensuales.',
+        id: 1,
+        img: '/Admin panel.png',
+        title: 'Panel de administración',
+        href: '#',
+        icon: ['ReactJsIcon, NodeJSIcon, PostgreSQLIcon'],
+        buttontxt: 'En proceso...',
+        description: 'CRUD para panel de administración usando tecnologías de v0.dev para el diseño y render.com para hosting ',
     },
     {
-        id: 3,
+        id: 2,
         img: '/galeria.png',
         title: 'Galería de Fotos',
         href: 'https://unagaleriadefotos.netlify.app/',
@@ -20,13 +20,22 @@ export const projects = [
         description: 'Galería de imágenes integrada con la API de Unsplash, mostrando fotografías de alta calidad.',
     },
     {
-        id: 4,
+        id: 3,
         img: '/tienda.png',
         title: 'Random Store',
         href: 'https://ramdomstore.netlify.app/',
         icon: ['ReactJsIcon'],
         buttontxt: 'Visitar el sitio',
         description: 'Plataforma de comercio electrónico construida con React y estilizada con Tailwind, ofreciendo una experiencia de compra dinámica. ',
+    },
+    {
+        id: 4,
+        img: '/metas.png',
+        title: 'Metas app',
+        href: 'https://github.com/Sie7he/academiaX',
+        icon: ['NodeJSIcon', 'ReactJsIcon', 'PostgreSQLIcon'],
+        buttontxt: 'Visitar Repositorio',
+        description: 'Aplicación interactiva CRUD para el registro y seguimiento de metas diarias, semanales y mensuales.',
     },
 
    
