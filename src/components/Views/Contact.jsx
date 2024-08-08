0ee154c71c7f0a5ac777a620fe30b77068de7d46
Sie7he <diegoescurraromero@gmail.com>
2024-08-08 00:36:09 -0400

    fix contact form

--- src/components/Views/Contact.jsx
+++ src/components/Views/Contact.jsx
@@ -1,46 +1,26 @@
 import { useForm, ValidationError } from "@formspree/react";
-import { useEffect, useState } from "react";
 
 export const Contact = () => {
   const [state, handleSubmit] = useForm("xaygqawn");
-  const [isOpen, setIsOpen] = useState(false);
 
 
-  useEffect(() => {
-    if (state.succeeded) {
-     setIsOpen(true)
-    }
-   }, [state.succeeded])
-
-  if (state.succeeded && isOpen) {
+  if (state.succeeded ) {
 
     return (
       <>
-        <div
-          className="fixed inset-0 bg-gray-600 bg-opacity-50 overflow-y-auto h-full w-full"
-          id="my-modal"
-        ></div>
-        <div className="fixed top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 bg-white w-1/3 shadow-lg rounded-lg">
-          <div className="p-4 border-b">
-            <h3 className="text-lg font-semibold text-gray-800">
+   
+          <div className="p-4 border-b text-gray-300 text-center">
+            <h3 className="text-lg font-semibold ">
               Mensaje Enviado
             </h3>
           </div>
-          <div className="p-4">
-            <p className="text-gray-600">
-              Gracias por tu mensaje. Nos pondremos en contacto contigo a la
-              brevedad.
+          <div className="p-4 text-center">
+            <p>
+              Gracias por tu mensaje. Me pondré en contacto contigo a la brevedad.
             </p>
           </div>
-          <div className="p-4 border-t flex justify-end">
-            <button
-              onClick={setIsOpen(false)}
-              className="px-4 py-2 bg-blue-500 text-white text-sm font-medium rounded hover:bg-blue-700 focus:outline-none focus:bg-blue-700"
-            >
-              Cerrar
-            </button>
-          </div>
-        </div>
+         
+        
       </>
     );
   }
