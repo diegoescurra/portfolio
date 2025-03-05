d3f3a35c2e28407bfcd458e84dbed7931685236b
Sie7he <diegoescurraromero@gmail.com>
2025-03-05 01:22:58 -0300

    add project veterinaria

--- src/data/projects.js
+++ src/data/projects.js
@@ -2,6 +2,15 @@
 export const projects = [
     {
         id: 1,
+        img: '/veterinaria.png',
+        gif: '/veterinaria.png',
+        title: 'Veterinaria App',
+        icon: ['SpringIcon', 'ReactJsIcon', 'MySQLIcon'],
+        description: 'Proyecto para la gestión de una veterinaria usando microservicios con Spring Cloud. Proyecto en proceso... ',
+        
+    },
+    {
+        id: 2,
         img: '/ventas.png',
         gif: '/ventas.png',
         title: 'API Ventas',
@@ -11,7 +20,7 @@ export const projects = [
         description: "API realizada en Java utilizando el framework de Spring para representar una gestión de un negocio.",
     },
     {
-        id: 2,
+        id: 3,
         img: '/Admin panel.png',
         gif: '/panel_gif.gif',
         title: 'Panel Administración',
@@ -21,18 +30,7 @@ export const projects = [
         github : 'https://github.com/Sie7he/admin_panel',
         description: "Panel de administración que generalmente uso en mis proyectos. Esta aplicación está usando un servidor gratis, por lo que tardará en iniciar.",
     },
-   
-    {
-        id: 3,
-        img: '/galeria.png',
-        gif: '/galeria.gif',
-        title: 'Galería de Fotos',
-        href: 'https://unagaleriadefotos.netlify.app/',
-        icon: ['ReactJsIcon'],
-        buttontxt: 'Ver',
-        github : 'https://github.com/Sie7he/gallery',
-        description: "Galería de imagenes que mezcla diseños de grilla y usa la transferencia entre páginas (Abrir en Google Chrome).",
-    },
+
     {
         id: 4,
         img: '/metas.png',
@@ -43,14 +41,6 @@ export const projects = [
         github: 'https://github.com/Sie7he/academiaX',
         description: 'Aplicación interactiva CRUD para el registro y seguimiento de metas diarias, semanales y mensuales (Proyecto de AcademiaX).',
     },
-    {
-        id: 6,
-        img: '/Hills.png',
-        gif: '/Hills.png',
-        title: 'Hills Bienes Raíces',
-        icon: ['NodeJSIcon', 'MySQLIcon', 'JQueryIcon'],
-        description: 'Anterior sitio de Altomonte Bienes Raíces',
-    },
-
+ 
    
 ];
